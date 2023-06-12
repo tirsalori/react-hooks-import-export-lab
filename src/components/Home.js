1577ee1e26847861bec582851556309adffc204b
Tirsa St. Fort <lifelovelori@gmail.com>
2023-06-12 15:02:11 -0700

    Complete lab

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,6 @@
 import React from "react";
+import {username} from "/Users/tirsa/Development/code/phase-2/react-hooks-import-export-lab/src/data/user.js"
+import {city} from "/Users/tirsa/Development/code/phase-2/react-hooks-import-export-lab/src/data/user.js"
 
 function Home() {
   return (
@@ -9,3 +11,5 @@ function Home() {
     </div>
   );
 }
+
+export default Home
